896ebbd8e143166e5ac053b0b323cb11b7a5208b
Shiva <st022781@gmail.com>
2025-06-16 15:43:12 +0400

    add dashboard

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 import { Providers } from "@/components/providers"
+import { ThemeProvider } from "@/components/providers/theme-provider"
+
 
 const fontSans = Geist({
   subsets: ["latin"],
@@ -23,7 +25,14 @@ export default function RootLayout({
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         <Providers>{children}</Providers>
+        </ThemeProvider>
       </body>
     </html>
   )
