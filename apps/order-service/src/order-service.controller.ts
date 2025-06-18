50e9a1bd5ba0915c8858f0e7fdedbdc7039e0036
AlexisLUN <sixela.lunel@gmail.com>
2025-06-18 11:48:57 +0200

    changement pour le suivie des commandes, dans order-service config et service, j'ai envie de crever et de crever des gens putain

--- apps/order-service/src/order-service.controller.ts
+++ apps/order-service/src/order-service.controller.ts
@@ -1,6 +1,9 @@
 import { Body, Controller, Get, Param, Post, Put, Patch } from '@nestjs/common';
 import { OrderServiceService } from './order-service.service';
 import { CreateOrderDto, UpdateOrderDto } from './dto/order.dto';
+import { OrderStatus } from 'libs/database/entities/order_status.enum';
+import { Order } from 'libs/database/entities/order.entity';
+import { NotFoundException } from '@nestjs/common';
 
 @Controller('orders')
 export class OrderServiceController {
@@ -26,5 +29,35 @@ export class OrderServiceController {
     return this.orderServiceService.getOrderById(id);
   }
   
+  @Patch(':id/accept-restaurant')
+  acceptByRestaurant(@Param('id') id: string) {
+    return this.orderServiceService.changeStatus(id, OrderStatus.ACCEPTED_RESTAURANT);
+  }
+
+  @Patch(':id/accept-delivery')
+  acceptByDelivery(@Param('id') id: string, @Body() body: { deliveryId: number }) {
+    return this.orderServiceService.acceptDelivery(id, body.deliveryId); 
+  }
+
+  @Patch(':id/preparing')
+  setPreparing(@Param('id') id: string) {
+    return this.orderServiceService.changeStatus(id, OrderStatus.PREPARING);
+  }
+
+  @Patch(':id/out-for-delivery')
+  setOutForDelivery(@Param('id') id: string) {
+    return this.orderServiceService.changeStatus(id, OrderStatus.OUT_FOR_DELIVERY);
+  }
+
+  @Patch(':id/delivered')
+  setDelivered(@Param('id') id: string) {
+    return this.orderServiceService.changeStatus(id, OrderStatus.DELIVERED);
+  }
+
+  @Get('status/:status')
+  getOrdersByStatus(@Param('status') status: OrderStatus) {
+    return this.orderServiceService.getOrdersByStatus(status);
+  }
+
 
 }
